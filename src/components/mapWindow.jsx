229e3e9553883bf2370133078e868138913fb358
Julio <ja.samaniegom@gmail.com>
2022-09-23 22:15:22 -0500

    search and fly to json placeholder data

--- src/components/mapWindow.jsx
+++ src/components/mapWindow.jsx
@@ -5,6 +5,7 @@ import { Icon } from 'leaflet';
 import { useState} from 'react';
 import FlyToButton from './flyToButton';
 import Search from './search';
+import Search2 from './search2';
 
 ///////////=============usar datos remotos
 delete L.Icon.Default.prototype._getIconUrl;
@@ -16,15 +17,15 @@ L.Icon.Default.mergeOptions({
 });
 
 const icon = new Icon({
-  iconSize: [35, 35],
+  iconSize: [30, 30],
   iconUrl: '/user-marker.svg',
-  iconAnchor: [17, 35]
+  iconAnchor: [15, 30]
 })
 
 const icon2 = new Icon({
-  iconSize: [30,30],
+  iconSize: [24,24],
   iconUrl: '/location-marker.svg',
-  iconAnchor: [15, 30]
+  iconAnchor: [12, 24]
 })
 
 const locations = [
@@ -34,7 +35,16 @@ const locations = [
   {id: 4, name: "Las Vegas" , coords: [36.104513967513135, -115.17950901317117]},
   {id: 5, name: "Cancun" , coords: [21.13425337242669, -86.74584281344949]},
   {id: 6, name: "Mazatlan" , coords: [23.25557877887862, -106.45852682074252]},
-  {id: 7, name: "Guadalajara" , coords: [20.703870624056464, -103.32770485383456]}
+  {id: 7, name: "Guadalajara" , coords: [20.703870624056464, -103.32770485383456]},
+  {id: 8, name: "Austin" , coords: [30.267407516061283, -97.76828579010953]},
+  {id: 9, name: "Monterrey" , coords: [25.72431724557821, -100.31410573393214]},
+  {id: 10, name: "CDMX" , coords: [19.440687237838386, -99.20277849047537]},
+  {id: 11, name: "Chihuahua" , coords: [28.705843398355437, -106.13502448941327]},
+  {id: 12, name: "Guanajuato" , coords: [21.01862178572715, -101.25756650176406]},
+  {id: 13, name: "Rio Grande" , coords: [23.71594422840233, -102.90808033103946]},
+  {id: 14, name: "Poza Rica" , coords: [20.548216555360177, -97.45915332832692]},
+  {id: 15, name: "Houston" , coords: [29.68531899090328, -95.41242027481584]},
+  {id: 15, name: "South Padre Island" , coords: [26.090899209291972, -97.16508349460821]}
 ]
 
 
@@ -48,6 +58,7 @@ const MapWindow = ({info}) => {
     <div className="map-window">
       <h2>JULIO'S JOURNEY</h2>
       <Search locations={locations} map={map}/>
+      <Search2 info={info} map={map}/>
       <MapContainer center={center.coords} zoom={4} scrollWheelZoom={false} className="map-map" ref={setMap}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -77,14 +88,14 @@ const MapWindow = ({info}) => {
           )
         })}
       </MapContainer>
-      {
+      {/* {
         locations.map(location => {
           return(
             <FlyToButton key={location.id} map={map} location={location}/>
           )
         })
       }
-      <FlyToButton key={center.id} map={map} location={center}/>
+      <FlyToButton key={center.id} map={map} location={center}/> */}
     </div>
   )
 }
---
+++ src/components/search2.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const Search2 = ({info, map}) => {  //change
+
+  const [textSearched, setTextSearched] = useState('');
+  const [matches, setMatches] = useState([]);
+
+  const textUpdate = e => {
+    setTextSearched(e.target.value)
+    filtered(e.target.value)
+  }
+    const filtered = (searchTerm) => {
+      var searchResult = info.filter((user)=>{
+        if(user.name.toString().toLowerCase().includes(searchTerm.toLowerCase())){
+          return user
+        }
+      });
+      setMatches(searchResult)
+    }
+  console.log(matches)
+
+  const flyTo2 = (e) => {
+    e.preventDefault()
+    for(let i = 0; i < info.length; i++){
+      if(textSearched === info[i].name.toString()){
+        console.log(map);
+        map.flyTo([info[i].address.geo.lat, info[i].address.geo.lng], 14);
+      }
+    }
+  }
+  
+
+  return (
+    <div className='search-section'>
+      <form onSubmit={flyTo2}>
+        <input onChange={textUpdate} value={textSearched}/>
+        <button>Go</button>
+      </form>
+      <ul className='search-matches'>
+        {
+          matches.map(match => {
+            return(
+              <li key={match.id} className='search-match' onClick={()=>{
+                setTextSearched(match.name);
+                setMatches([]);
+                }}>
+                <span>{match.name}</span>
+              </li>
+            ) 
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Search2
